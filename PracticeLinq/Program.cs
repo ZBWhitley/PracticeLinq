db458f75ce7037d5d4c1d42ac5eb61af9b1ebb04
Zachary Whitley <zbwhitley@gmail.com>
2022-03-03 17:13:47 -0600

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vGList = new List<string> {"Mario", "Donkey Kong", "Phantasy Star", "League of Legends", "Rocket League", "Lost Ark", "Horizon Zero Down"};
+
+            vGList.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x) );
+
+            //foreach (var vG in vGSorted)
+            //{
+            //    Console.WriteLine(vG);
+            //}
+
+        }
+    }
+}
